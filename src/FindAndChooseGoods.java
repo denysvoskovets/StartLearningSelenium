02069775025ebce1cd15e651397ad079069e0b8f
denysvoskovets <denisvoskovets@gmail.com>
2016-08-24 14:26:34 +0300

    addedMethodHotlineBuying

---
+++ src/FindAndChooseGoods.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class FindAndChooseGoods {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://hotline.ua");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		driver.findElement(By.xpath("//*[@id='lv-1-193']/a/b")).click();
+		
+		driver.findElement(By.xpath("//div[@class='text']")).click();
+		//WebElement dropdown = driver.findElement(By.xpath("//div[text()='Выберите категорию']")); //будем выбирать категорию из дроп списка
+		driver.findElement(By.xpath("//ul[@style='list-style:none']/li[.='TV-тюнеры']")).click();
+		driver.findElement(By.xpath("//a[@class='but-box cell but-blue fl js-view-result g_statistic']")).click();
+		driver.findElement(By.xpath("//*[@id='catalogue']/div[5]/div[1]/div/div/div[3]/div[2]/div[2]/a")).click();
+		
+		
+		
+
+	}
+
+}
